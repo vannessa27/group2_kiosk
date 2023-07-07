7b0d747ddf0b82343b408bbb2a611e17173cea43
@vannessa27 <tipqc@Q5203-13>
2023-07-07 13:16:42 +0800

    group creativitea project

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -14,7 +14,7 @@ public class Kiosk {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here TEST
     }
     
 }
